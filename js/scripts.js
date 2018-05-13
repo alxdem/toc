dec6f2bf968e9b28ef11980c71d9ac20838cd54c
alxdem <dr.a.lekter@gmail.com>
2018-05-14 00:29:07 +0300

    Заменил forEach на for. Исправил append на appendChild. Теперь работает в IE11 тоже

--- js/scripts.js
+++ js/scripts.js
@@ -17,30 +17,26 @@ var tocInit = function() {
   var tocList = createNewElement('ul', 'toc-list');
   var tocTitleNumb = 1; // Порядковый номер для задания ID заголовкам в тексте
 
-  arrTocElements.forEach(function(item, arrTocElements){
+  for(var i = 0; i < arrTocElements.length; i++) {
     var tocListElement = createNewElement('li', 'toc-list-item'); // Создаем элемент списка в оглавлении
     var tocListLink = createNewElement('a', 'toc-list-link'); //Создание ссылки-якоря на заголовок
-    var tocItemTag = item.tagName; // Получаем тег заголовка (h2 или h3 и т. д.)
-    var itemTitle = item;
+    var tocItemTag = arrTocElements[i].tagName; // Получаем тег заголовка (h2 или h3 и т. д.)
+    var itemTitle = arrTocElements[i];
     var tocTitleId = 'toc-title-' + tocTitleNumb; // Задаем ID заголовкам в тексте
     itemTitle.id = tocTitleId;
 
     tocListElement.classList.add('toc-list-item-' + tocItemTag); // Задаем класс для стилизации, в зависимости от уровня заголовка
     tocListLink.href = '#' + tocTitleId;
-    tocListLink.textContent = item.textContent;
+    tocListLink.textContent = arrTocElements[i].textContent;
 
-    tocListElement.append(tocListLink);
-    tocList.append(tocListElement);
+    tocListElement.appendChild(tocListLink);
+    tocList.appendChild(tocListElement);
 
     tocTitleNumb++; // Увеличиваем порядковый номер на 1 для следующего заголовка
-  });
+  }
 
-  tocHeader.append(tocList);
-
-  console.log(tocList);
+  tocHeader.appendChild(tocList);
 
 }
 
 tocInit();
-
-console.log(arrTocElements);
